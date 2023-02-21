ce6ba37c7066238a5ce7bbdf1bc29ec9c014d748
Kin-Wai Lam <kinwai.lam730@gmail.com>
2023-02-21 16:28:24 -0500

    Created a new expense item component

---
+++ src/components/ExpenseItem.component.jsx
@@ -0,0 +1,13 @@
+const ExpenseItem = () =>{
+    return(
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Expense Item</h2>
+                <div>Expense Amount</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
