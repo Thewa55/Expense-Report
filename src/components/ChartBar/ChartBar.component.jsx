4755f5ce0130d3aa039dab52f643dfa93fc5bc32
Kin-Wai Lam <kinwai.lam730@gmail.com>
2023-03-01 18:59:47 -0500

    Moved the styled component outside of the component to remove console errors

--- src/components/ChartBar/ChartBar.component.jsx
+++ src/components/ChartBar/ChartBar.component.jsx
@@ -1,39 +1,39 @@
-import React, {useState} from "react";
+import React from "react";
 // import './ChartBar.css';
 import styled from "styled-components";
 
-const ChartBar = props => { 
+const Bar = styled.div`
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
 
-    const Bar = styled.div`
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        
-        & .chart-bar__inner {
-            height: 100%;
-            width: 100%;
-            border: 1px solid #313131;
-            border-radius: 12px;
-            background-color: #c3b4f3;
-            overflow: hidden;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-        }
-        
-        & .chart-bar__fill {
-            background-color: ${props => props.tallest ? 'red' : '#4826b9'};
-            width: 100%;
-            transition: all 0.3s ease-out;
-        }
-        
-        & .chart-bar__label {
-            font-weight: bold;
-            font-size: 0.5rem;
-            text-align: center;
-        }
-    `
+& .chart-bar__inner {
+    height: 100%;
+    width: 100%;
+    border: 1px solid #313131;
+    border-radius: 12px;
+    background-color: #c3b4f3;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+}
+
+& .chart-bar__fill {
+    background-color: ${props => props.tallest ? 'red' : '#4826b9'};
+    width: 100%;
+    transition: all 0.3s ease-out;
+}
+
+& .chart-bar__label {
+    font-weight: bold;
+    font-size: 0.5rem;
+    text-align: center;
+}
+`
+
+const ChartBar = props => { 
 
     let barFillHeight = '0%';
     let tallest = false;
