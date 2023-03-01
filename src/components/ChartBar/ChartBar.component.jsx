cb6219d702742c72969d4b53d6426b9888f9e26a
Kin-Wai Lam <kinwai.lam730@gmail.com>
2023-03-01 17:48:12 -0500

    Added styled components dependency and restyled ChartBar using styled components

--- src/components/ChartBar/ChartBar.component.jsx
+++ src/components/ChartBar/ChartBar.component.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-import './ChartBar.css';
+// import './ChartBar.css';
+import styled from "styled-components";
 
 const ChartBar = props => { 
+
+    const Bar = styled.div`
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+        & .chart-bar__inner {
+            height: 100%;
+            width: 100%;
+            border: 1px solid #313131;
+            border-radius: 12px;
+            background-color: #c3b4f3;
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+        }
+        
+        & .chart-bar__fill {
+            background-color: #4826b9;
+            width: 100%;
+            transition: all 0.3s ease-out;
+        }
+        
+        & .chart-bar__label {
+            font-weight: bold;
+            font-size: 0.5rem;
+            text-align: center;
+        }
+    `
+
     let barFillHeight = '0%';
 
     if(props.maxValue > 0){
@@ -9,7 +42,8 @@ const ChartBar = props => {
     }
 
     return ( 
-        <div className="chart-bar">
+        // <div className="chart-bar">
+        <Bar>
             <div className="chart-bar__inner">
                 <div className="chart-bar__fill" style={{height: barFillHeight}}>
                 </div>   
@@ -17,7 +51,9 @@ const ChartBar = props => {
             <div className="chart-bar__label">
                 {props.label}
             </div>
-        </div>
+        </Bar>
+
+        // </div>
     )
 }
 
